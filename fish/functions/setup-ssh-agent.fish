6df4e90c2dde2bd4677d967b2190cc32361cf7d8
Arnaldo Garcia Rincon <agarrcia@amazon.com>
2022-01-09 04:46:27 +0000

    fish: fix setup-ssh-agent

--- fish/functions/setup-ssh-agent.fish
+++ fish/functions/setup-ssh-agent.fish
@@ -1,16 +1,20 @@
 function setup-ssh-agent
-  # SSH_SHARED_SESSION is a universal variable to share the socket across all the fish sessions
-  if test -z $SSH_SHARED_SESSION; or test -n "$SSH_SHARED_SESSION"; and test ! -e "$SSH_SHARED_SESSION"
+  # SSH_SHARED_SESSION is a sentry file to share the socket across all the fish sessions
+  set -gx SSH_SHARED_SESSION /run/user/(id -u)/ssh-created
+  if test ! -e $SSH_SHARED_SESSION; or test ! -e (read < $SSH_SHARED_SESSION)
     # Erease variables, to make sure they are not set already
     set -e SSH_AGENT_PID
     set -e SSH_AUTH_SOCK
 
+    # Kill any tangling agents
+    pkill ssh-agent
+
     eval (ssh-agent -c)
 
     # Update SSH_SHARED_SESSION
-    set -U SSH_SHARED_SESSION $SSH_AUTH_SOCK
+    echo $SSH_AUTH_SOCK > $SSH_SHARED_SESSION
   else
-    set -gx SSH_AUTH_SOCK $SSH_SHARED_SESSION
+    set -gx SSH_AUTH_SOCK (read < $SSH_SHARED_SESSION)
   end
 
   # At this point, the agent should exist, now add the keys
