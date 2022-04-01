81ea0d375a9a71e7335324b217984065ea602e1d
Arnaldo Garcia Rincon <agarrcia@amazon.com>
2022-04-01 15:02:58 -0700

    fish: use local fzf installation when available
    Signed-off-by: Arnaldo Garcia Rincon <agarrcia@amazon.com>

--- fish/functions/setup_fzf_key_bindings.fish
+++ fish/functions/setup_fzf_key_bindings.fish
@@ -1,6 +1,9 @@
 function setup_fzf_key_bindings
+  set --local FZF_KEY_BINDINGS_LOCAL_PATH $HOME/.local/share/fzf/shell/key-bindings.fish
   set --local FZF_KEY_BINDINGS_PATH /usr/share/fzf/shell/key-bindings.fish
-  if test -e $FZF_KEY_BINDINGS_PATH
+  if test -e $FZF_KEY_BINDINGS_LOCAL_PATH
+    source $FZF_KEY_BINDINGS_LOCAL_PATH
+  else if test -e $FZF_KEY_BINDINGS_PATH
     source $FZF_KEY_BINDINGS_PATH
   end
 
